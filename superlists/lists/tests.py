b872ddc264c0d831aba6cce50070a64f9ec62b8b
changyu <changyu.park1@gmail.com>
2021-09-21 12:40:40 +0900

    p.33 PS E:\books\pythonCleanCodeTDD\superlists> python .\manage.py test Creating test database for alias 'default'... . ---------------------------------------------------------------------- Ran 1 test in 0.001s
    OK
    Destroying test database for alias 'default'...

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -13,4 +13,4 @@ class SmokeTest(TestCase):
 class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
-        self.assertEqual(found.func.home_page)
+        self.assertEqual(found.func, home_page)
