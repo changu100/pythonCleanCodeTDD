27e9c71987cdc4ff0d926a70fb58109614761706
changyu <changyu.park1@gmail.com>
2021-09-22 17:52:23 +0900

    p.44 PS E:\books\pythonCleanCodeTDD> python .\functional_tests.py E ====================================================================== ERROR: test_can_start_a_list_and_retriee_it_later (__main__.NewVisitorTest) ---------------------------------------------------------------------- Traceback (most recent call last):   File ".\functional_tests.py", line 20, in test_can_start_a_list_and_retriee_it_later     header_text= self.browser.find_element_by_tag_name('h1').text   File "C:\python\python34\lib\site-packages\selenium\webdriver\remote\webdriver.py", line 530, in find_element_by_tag_name     return self.find_element(by=By.TAG_NAME, value=name)   File "C:\python\python34\lib\site-packages\selenium\webdriver\remote\webdriver.py", line 978, in find_element     'value': value})['value']   File "C:\python\python34\lib\site-packages\selenium\webdriver\remote\webdriver.py", line 321, in execute     self.error_handler.check_response(response)   File "C:\python\python34\lib\site-packages\selenium\webdriver\remote\errorhandler.py", line 242, in check_response     raise exception_class(message, screen, stacktrace) selenium.common.exceptions.NoSuchElementException: Message: Unable to locate element: h1
    ----------------------------------------------------------------------
    Ran 1 test in 8.533s
    
    FAILED (errors=1)

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -16,6 +17,28 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get('http://localhost:8000')
 
         self.assertIn('To-Do',self.browser.title)
+        header_text= self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do',header_text)
+
+        #그녀는 바로 작업을 추가하기로한다. 
+        inputbox = self.browser.find_element_by_id('id_new_item')
+
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            '작업 아이템 입력'
+        )
+
+        # "공작깃털 사기"라고 테스트 상자에 입력한다
+        # (에디스의 취미는 날치 잡이용 그물을 만드는 것이다.)
+        inputbox.send_keys(Keys.ENTER)
+
+        table= self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: 공작깃털 사기' for row in rows)
+        )
+
+
         self.fail('Finish the test!')
 
 if __name__ == '__main__':
