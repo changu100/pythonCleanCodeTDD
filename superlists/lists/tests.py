e3c46e357518f83030745ec69afcad76d3f7509d
changyu <changyu.park1@gmail.com>
2021-09-27 16:55:51 +0900

    p 122 PS E:\books\pythonCleanCodeTDD\superlists> python .\manage.py test lists Creating test database for alias 'default'... ....... ---------------------------------------------------------------------- Ran 7 tests in 0.030s
    OK
    Destroying test database for alias 'default'...

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -38,18 +38,26 @@ class HomePageTest(TestCase):
 
 class ListViewTest(TestCase):
     def test_uses_list_template(self):
-        response= self.client.get('/lists/the-only-list-in-the-world/')
+        list_ = List.objects.create()
+        response = self.client.get('/lists/%d/'% (list_.id,))
         self.assertTemplateUsed(response, 'list.html')
 
-    def test_displays_all_list_items(self):
-        list_ = List.objects.create()
-        Item.objects.create(text='itemey 1', list=list_)
-        Item.objects.create(text='itemey 2', list=list_)
 
-        response= self.client.get('/lists/the-only-list-in-the-world/')
+    def test_displays_only_items_for_that_list(self):
+        correct_list = List.objects.create()
+        Item.objects.create(text='itemey 1', list=correct_list)
+        Item.objects.create(text='itemey 2', list=correct_list)
+        other_list = List.objects.create()
+        Item.objects.create(text='other list item 1', list=other_list)
+        Item.objects.create(text='other list item 2', list=other_list)
+
+        response = self.client.get('/lists/%d/' % (correct_list.id,))
 
         self.assertContains(response, 'itemey 1')
         self.assertContains(response, 'itemey 2')
+        self.assertNotContains(response, 'other list item 1')
+        self.assertNotContains(response, 'other list item 2')
+
 
 class NewListTest(TestCase):
     def test_saving_a_POST_request(self):
@@ -67,8 +75,8 @@ class NewListTest(TestCase):
             '/lists/new',
             data={'item_text':'신규 작업 아이템'}
         )
-        self.assertEqual(response.status_code,302)
-        self.assertRedirects(response,'/lists/the-only-list-in-the-world/')
+        new_list = List.objects.first()
+        self.assertRedirects(response,'/lists/%d/'% (new_list.id,))
 
 
 class ListAndItemModelsTest(TestCase):
