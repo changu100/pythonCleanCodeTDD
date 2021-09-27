3557fed59f5ea2f6dd471adda2f63a3426f059cf
changyu <changyu.park1@gmail.com>
2021-09-27 16:28:09 +0900

    p 118 PS E:\books\pythonCleanCodeTDD\superlists> python .\manage.py test lists Creating test database for alias 'default'... ....... ---------------------------------------------------------------------- Ran 7 tests in 0.026s
    OK
    Destroying test database for alias 'default'...

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -4,8 +4,8 @@ from django.template.loader import render_to_string
 from lists.views import home_page
 from django.test import TestCase
 from django.http import HttpRequest, request, response
+from lists.models import Item, List
 
-from lists.models import Item
 # Create your tests here.
 
 """
@@ -34,26 +34,6 @@ class HomePageTest(TestCase):
 
 
     
-class ItemModelTest(TestCase):
-
-    def test_saving_and_retrieving_items(self):
-        first_item = Item()
-        first_item.text = '첫 번째 아이템'
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = '두 번째 아이템'
-        second_item.save()
-
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(),2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-
-        self.assertEqual(first_saved_item.text,'첫 번째 아이템')
-        self.assertEqual(second_saved_item.text,'두 번째 아이템')
 
 
 class ListViewTest(TestCase):
@@ -62,8 +42,9 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response, 'list.html')
 
     def test_displays_all_list_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
+        list_ = List.objects.create()
+        Item.objects.create(text='itemey 1', list=list_)
+        Item.objects.create(text='itemey 2', list=list_)
 
         response= self.client.get('/lists/the-only-list-in-the-world/')
 
@@ -88,3 +69,33 @@ class NewListTest(TestCase):
         )
         self.assertEqual(response.status_code,302)
         self.assertRedirects(response,'/lists/the-only-list-in-the-world/')
+
+
+class ListAndItemModelsTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.list = list_
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.list = list_
+        second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(first_saved_item.list, list_)
+        self.assertEqual(second_saved_item.text, 'Item the second')
+        self.assertEqual(second_saved_item.list, list_)
